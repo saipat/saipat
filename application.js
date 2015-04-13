3cf559b8ab9d2e0d625d55a340c7ca9c5022a933
Sai Lakshmi <Sai@Macintosh.local>
2015-04-13 06:55:42 -0500

    jcarousel integration

---
+++ application.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+	$("#btn-1").click(function(){
+		$(".location").slideToggle("slow");
+	});
+
+	$("#sub").on("click",function(){
+		$(this).remove();
+		$("h5").text("Thanks for your comment. Your comment is submitted");
+	});
+
+	$(".lists li").hover(function(){
+		$("span").show();
+	});
+	
+	$("#ri li #app").click(function(){
+		$("#ri li #app").show();
+		$("#list1 li").slideToggle("slow");
+	});
+
+	$("#ri li #ent").click(function(){
+		$("#ent").show();
+		$("#list2 li").slideToggle("slow");
+	});
+
+	$("#ri li #des").click(function(){
+		$("#des").show();
+		$("#list3 li").slideToggle("slow");
+	});
+});
+
